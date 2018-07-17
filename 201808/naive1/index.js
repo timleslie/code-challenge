fd9c013bc43050b2e28dc31207bd7f85fe7f3b5d
Tim Leslie <timl@thinkmill.com.au>
2018-07-17 11:59:12 +1000

    Clean up naive bot

--- 201808/naive1/index.js
+++ 201808/naive1/index.js
@@ -7,105 +7,96 @@ const {
 	ACTIONS,
 } = require('../constants.js');
 
+class BOT {
+	OnTurn({ history, myCards, myCoins, otherPlayers, discardedCards }) {
+		let action = ACTIONS[ Math.floor( Math.random() * ACTIONS.length ) ];
+		const against = otherPlayers[ Math.floor( Math.random() * otherPlayers.length ) ].name;
+		const scores = otherPlayers.map(p => [p.coins * p.cards, p])
+		if( myCoins >= 7 ) {
+			action = 'couping';
+		} else if (myCards.includes('assassin') && myCoins >= 3) {
+			action = 'assassination';
+		} else if (myCards.includes('duke')) {
+			action = 'taking-3';
+		} else if (myCards.includes('ambassador')) {
+			action = 'swapping';
+		} else if (([...discardedCards, ...myCards]).filter(c => c === 'captain').length === 3 && myCards.includes('captain')) {
+			action = 'stealing';
+		} else if (([...discardedCards, ...myCards]).filter(c => c === 'ambassador').length === 3 && myCards.includes('captain')) {
+			action = 'stealing';
+		} else if (discardedCards.filter(c => c === 'duke').length === 3) {
+			action = 'foreign-aid';
+		} else {
+			action = 'taking-1';
+		}
 
-const OnTurn = ({ history, myCards, myCoins, otherPlayers, discardedCards }) => {
-	let action = ACTIONS[ Math.floor( Math.random() * ACTIONS.length ) ];
-	const against = otherPlayers[ Math.floor( Math.random() * otherPlayers.length ) ].name;
-	const scores = otherPlayers.map(p => [p.coins * p.cards, p])
-	if( myCoins >= 7 ) {
-		action = 'couping';
-	} else if (myCards.includes('assassin') && myCoins >= 3) {
-		action = 'assassination';
-	} else if (myCards.includes('duke')) {
-		action = 'taking-3';
-	} else if (myCards.includes('ambassador')) {
-		action = 'swapping';
-	} else if (([...discardedCards, ...myCards]).filter(c => c === 'captain').length === 3 && myCards.includes('captain')) {
-		action = 'stealing';
-	} else if (([...discardedCards, ...myCards]).filter(c => c === 'ambassador').length === 3 && myCards.includes('captain')) {
-		action = 'stealing';
-	} else if (discardedCards.filter(c => c === 'duke').length === 3) {
-		action = 'foreign-aid';
-	} else {
-		action = 'taking-1';
+		return {
+			action,
+			against,
+		};
 	}
 
-	return {
-		action,
-		against,
-	};
-};
+	OnChallengeActionRound({ history, myCards, myCoins, otherPlayers, discardedCards, action, byWhom, toWhom}) {
+		const cards = [...myCards, ...discardedCards];
+		switch (action) {
+			case 'taking-3':
+				if (cards.filter(c => c === 'duke').length === 3) {
+					return true
+				}
+				break;
+			case 'assassination':
+				if (cards.filter(c => c === 'assassin').length === 3) {
+					return true
+				}
+				break;
+			case 'stealing':
+				if (cards.filter(c => c === 'captain').length === 3) {
+					return true
+				}
+				break;
+			case 'swapping':
+				if (cards.filter(c => c === 'captain').length === 3) {
+					return true
+				}
+			break;
+	}
+		return false;
+	}
 
-const OnChallengeActionRound = ({ history, myCards, myCoins, otherPlayers, discardedCards, action, byWhom, toWhom }) => {
-	const cards = [...myCards, ...discardedCards];
-	switch (action) {
-		case 'taking-3':
-			if (cards.filter(c => c === 'duke').length === 3) {
-				return true
+	OnCounterAction({ history, myCards, myCoins, otherPlayers, discardedCards, action, byWhom }) {
+		if (action === 'foreign-aid' && myCards.includes('duke')) {
+			return 'duke';
+		}
+		if (action === 'assassination' && myCards.includes('contessa')) {
+			return 'contessa';
+		}
+		if (action === 'stealing') {
+			if (myCards.includes('captain')) {
+				return 'captain';
 			}
-			break;
-		case 'assassination':
-			if (cards.filter(c => c === 'assassin').length === 3) {
-				return true
+			if (myCards.includes('ambassador')) {
+				return 'ambassador';
 			}
-			break;
-		case 'stealing':
-			if (cards.filter(c => c === 'captain').length === 3) {
-				return true
-			}
-			break;
-		case 'swapping':
-			if (cards.filter(c => c === 'captain').length === 3) {
-				return true
-			}
-		break;
+		}
+		return false;
+	}
+
+	OnCounterActionRound({ history, myCards, myCoins, otherPlayers, discardedCards, action, byWhom, toWhom, card }) {
+		const cards = [...myCards, ...discardedCards];
+		if (cards.filter(c => c === card).length === 3) {
+			return true
+		}
+		return false;
+	}
+
+	OnSwappingCards({ history, myCards, myCoins, otherPlayers, discardedCards, newCards }) {
+		return newCards;
+	}
+
+	OnCardLoss({ history, myCards, myCoins, otherPlayers, discardedCards }) {
+		return myCards[ 0 ];
+	}
 }
-	return false;
-};
-
-const OnCounterAction = ({ history, myCards, myCoins, otherPlayers, discardedCards, action, byWhom }) => {
-	if (action === 'foreign-aid' && myCards.includes('duke')) {
-		return 'duke';
-	}
-	if (action === 'assassination' && myCards.includes('contessa')) {
-		return 'contessa';
-	}
-	if (action === 'stealing') {
-		if (myCards.includes('captain')) {
-			return 'captain';
-		}
-		if (myCards.includes('ambassador')) {
-			return 'ambassador';
-		}
-	}
-	return false;
-};
-
-const OnCounterActionRound = ({ history, myCards, myCoins, otherPlayers, discardedCards, action, card, byWhom, toWhom }) => {
-	const cards = [...myCards, ...discardedCards];
-	if (cards.filter(c => c === card).length === 3) {
-		console.info(cards, action, card, byWhom, toWhom);
-		return true
-	}
-	// console.info('COUNTER', action, card, byWhom, toWhom);
-	return false;
-};
-
-const OnSwappingCards = ({ history, myCards, myCoins, otherPlayers, discardedCards, newCards }) => {
-	return newCards;
-};
-
-const OnCardLoss = ({ history, myCards, myCoins, otherPlayers, discardedCards }) => {
-	// console.info(myCards);
-	return myCards[ 0 ];
-};
 
 
-module.exports = exports = {
-	OnTurn,
-	OnChallengeActionRound,
-	OnCounterAction,
-	OnCounterActionRound,
-	OnSwappingCards,
-	OnCardLoss,
-};
+module.exports = exports = BOT;
